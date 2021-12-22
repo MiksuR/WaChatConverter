c3423a9e26015c93af7bf5e1957cab7cea2190e4
Miksu Rankaviita <8172649+MiksuR@users.noreply.github.com>
2021-12-22 23:23:42 +0000

    Implement parsing

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,19 +13,30 @@ import qualified Data.Text.IO as TIO
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Lazy.Char8 as C
 
+import Data.Either.Combinators (maybeToRight)
+import Data.Maybe (isJust)
+import Data.Time
+
 data Message = Message {
-  date :: (), time :: (), sender :: T.Text, text :: T.Text
+  date :: LocalTime, sender :: BL.ByteString, text :: BL.ByteString
 } deriving Show
 
-data ParseError = ParseError !Int !String
+data ParseError = ParseError !Int !BL.ByteString
 
 errorMsg :: ParseError -> String
-errorMsg (ParseError line text) = "Could not parse line " ++ show line ++ ": " ++ text
+errorMsg (ParseError line text) = "Could not parse line " ++ show line ++ ": " ++ C.unpack text
 
 parseStr :: (Int, BL.ByteString) -> Either ParseError Message
-parseStr (line, str) = Right Message { date = (), time = (),
-                                       sender = T.pack "tester",
-                                       text = T.pack . C.unpack $ str }
+parseStr (line, str) = maybeToRight parseError $
+                        createMessage <$> date <*> sender
+  where
+    (dateStr, rest) = C.break ('-'==) str
+    date = parseTimeM True defaultTimeLocale "%-d.%-m.%-Y klo %H.%M" $ C.unpack dateStr :: Maybe LocalTime
+    (senderPart, textPart) = C.break (':'==) rest
+    sender = let s = C.drop 2 senderPart in if C.null s then Nothing else Just s
+    text = C.drop 2 textPart
+    createMessage d s = Message d s text
+    parseError = ParseError line str
 
 parseMessages :: BL.ByteString -> Either ParseError [Message]
 parseMessages str = mapM parseStr $ zip [1..] (C.lines str)
