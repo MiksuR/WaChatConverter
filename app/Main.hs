d4e3946048f8db267d69d0a060c801bdcf1f4dcd
Miksu Rankaviita <8172649+MiksuR@users.noreply.github.com>
2021-12-23 22:27:45 +0000

    Build blocks and compile to pdf

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,14 @@ module Main where
 
 import Lib
 
-import qualified Data.ByteString.Lazy as BL
+import Control.Monad.IO.Class (liftIO)
+import Data.Default (def)
+import Text.Pandoc
+import Text.Pandoc.PDF
 import System.Environment (getArgs)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Text as T
 
 extractFirstArg :: [String] -> IO String
 extractFirstArg [] = error "No arguments provided"
@@ -11,8 +17,14 @@ extractFirstArg (x:_) = pure x
 
 main :: IO ()
 main = do
+  templRaw <- runIO (getTemplate "template.tex") >>= handleError
+  Right templ <- compileTemplate "" templRaw :: IO (Either String (Template T.Text))
   chatsPath <- getArgs >>= extractFirstArg
-  chats <- BL.readFile chatsPath
+  chats <- B.readFile chatsPath
+  -- TODO: refactor, skip unnecesary parsing
   parsed <- either (error . errorMsg) pure (parseMessages chats)
   let doc = createDocument $ parsed
-  print $ parsed
+  pdf <- runIO (makePDF "pdflatex" [] writeLaTeX
+                (def {writerTemplate = Just templ}) doc) >>= handleError
+  case pdf of Right b -> BL.writeFile "out.pdf" b
+              Left e -> print e
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Lib
     (
       parseMessages,
@@ -8,38 +6,64 @@ module Lib
     ) where
 
 import Text.Pandoc
+import Text.Pandoc.Builder
+import Data.Sequence ( Seq(Empty) )
+import Data.List (foldl')
+import Data.Text.Encoding
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString.Lazy.Char8 as C
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
 
 import Data.Either.Combinators (maybeToRight)
 import Data.Maybe (isJust)
 import Data.Time
 
 data Message = Message {
-  date :: LocalTime, sender :: BL.ByteString, text :: BL.ByteString
+  time :: LocalTime, sender :: B.ByteString, msgText :: B.ByteString
 } deriving Show
 
-data ParseError = ParseError !Int !BL.ByteString
+data ParseError = ParseError !Int !B.ByteString
+
+type BlockAcc = (Blocks, [LocalTime], [LocalTime], B.ByteString)
+emptyAcc = (mempty, [], [], C.pack "")
 
 errorMsg :: ParseError -> String
 errorMsg (ParseError line text) = "Could not parse line " ++ show line ++ ": " ++ C.unpack text
 
-parseStr :: (Int, BL.ByteString) -> Either ParseError Message
+parseStr :: (Int, B.ByteString) -> Either ParseError Message
 parseStr (line, str) = maybeToRight parseError $
                         createMessage <$> date <*> sender
   where
     (dateStr, rest) = C.break ('-'==) str
-    date = parseTimeM True defaultTimeLocale "%-d.%-m.%-Y klo %H.%M" $ C.unpack dateStr :: Maybe LocalTime
+    date = parseTimeM True defaultTimeLocale "%-d.%-m.%-Y klo %-H.%M" $ C.unpack dateStr :: Maybe LocalTime
     (senderPart, textPart) = C.break (':'==) rest
     sender = let s = C.drop 2 senderPart in if C.null s then Nothing else Just s
     text = C.drop 2 textPart
     createMessage d s = Message d s text
     parseError = ParseError line str
 
-parseMessages :: BL.ByteString -> Either ParseError [Message]
+parseMessages :: B.ByteString -> Either ParseError [Message]
 parseMessages str = mapM parseStr $ zip [1..] (C.lines str)
 
+updateAcc :: BlockAcc -> Message -> BlockAcc
+updateAcc (blocks, months, msgs, lSender) msg = (nBlock, nMonths, nMsgs, nSender)
+  where
+    nSender = sender msg
+    nMsgs = if lSender == nSender then msgs else msgs ++ [time msg]
+    (_, prevMonth, _) = case msgs of [] -> (0, 0, 0)
+                                     x -> toGregorian . localDay . last $ x
+    (_, curMonth, _) = toGregorian . localDay . time $ msg
+    nMonths = if prevMonth == curMonth then months else months ++ [time msg]
+    monthPar = if prevMonth == curMonth then mempty
+               else header 1 (text (T.pack $ show curMonth))
+    senderPar = if lSender == nSender then mempty
+                else para $ text (decodeUtf8 nSender `T.append` T.pack ": ")
+    messageText = para $ text (decodeUtf8 $ msgText msg)
+    nBlock = blocks <> monthPar <> senderPar <> messageText
+
 createDocument :: [Message] -> Pandoc
-createDocument (x:xs) = undefined
+createDocument msgs = doc $ blocks
+  where
+    toc = undefined
+    (blocks, months, _, _) = foldl' updateAcc emptyAcc msgs
