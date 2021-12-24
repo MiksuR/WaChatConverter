3dca681757b0b713eab5dc067be5276506eacb4f
Miksu Rankaviita <8172649+MiksuR@users.noreply.github.com>
2021-12-24 22:01:46 +0000

    Refine message block format

--- app/Main.hs
+++ app/Main.hs
@@ -10,6 +10,7 @@ import System.Environment (getArgs)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as E
 
 extractFirstArg :: [String] -> IO String
 extractFirstArg [] = error "No arguments provided"
@@ -20,10 +21,15 @@ main = do
   templRaw <- runIO (getTemplate "template.html") >>= handleError
   Right templ <- compileTemplate "" templRaw :: IO (Either String (Template T.Text))
   chatsPath <- getArgs >>= extractFirstArg
+  -- TODO: Try implement lazy parsing
   chats <- B.readFile chatsPath
   -- TODO: refactor, skip unnecesary parsing
   parsed <- either (error . errorMsg) pure (parseMessages chats)
   let doc = createDocument $ parsed
+  {--
+  pdf <- runIO (writeHtml5String (def {writerTemplate = Just templ}) doc) >>= handleError
+  B.writeFile "out.html" $ E.encodeUtf8 pdf
+  --}
   pdf <- runIO (makePDF "wkhtmltopdf" [] writeHtml5String
                 (def {writerTemplate = Just templ}) doc) >>= handleError
   case pdf of Right b -> BL.writeFile "out.pdf" b
--- src/Lib.hs
+++ src/Lib.hs
@@ -7,8 +7,8 @@ module Lib
 
 import Text.Pandoc
 import Text.Pandoc.Builder
-import Data.Sequence ( Seq(Empty) )
 import Data.List (foldl')
+import Data.Sequence (Seq)
 import Data.Text.Encoding
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
@@ -36,7 +36,8 @@ parseStr (line, str) = maybeToRight parseError $
                         createMessage <$> date <*> sender
   where
     (dateStr, rest) = C.break ('-'==) str
-    date = parseTimeM True defaultTimeLocale "%-d.%-m.%-Y klo %-H.%M" $ C.unpack dateStr :: Maybe LocalTime
+    date = parseTimeM True defaultTimeLocale "%-d.%-m.%-Y klo %-H.%M" $
+                            C.unpack dateStr :: Maybe LocalTime
     (senderPart, textPart) = C.break (':'==) rest
     sender = let s = C.drop 2 senderPart in if C.null s then Nothing else Just s
     text = C.drop 2 textPart
@@ -51,16 +52,29 @@ updateAcc (blocks, months, msgs, lSender) msg = (nBlock, nMonths, nMsgs, nSender
   where
     nSender = sender msg
     nMsgs = if lSender == nSender then msgs else msgs ++ [time msg]
-    (_, prevMonth, _) = case msgs of [] -> (0, 0, 0)
-                                     x -> toGregorian . localDay . last $ x
-    (_, curMonth, _) = toGregorian . localDay . time $ msg
+    (_, prevMonth, prevDay) = case msgs of
+                                [] -> (0, 0, 0)
+                                x -> toGregorian . localDay . last $ x
+    (_, curMonth, curDay) = toGregorian . localDay . time $ msg
     nMonths = if prevMonth == curMonth then months else months ++ [time msg]
-    monthPar = if prevMonth == curMonth then mempty
-               else header 1 (text (T.pack $ show curMonth))
-    senderPar = if lSender == nSender then mempty
-                else para $ text (decodeUtf8 nSender `T.append` T.pack ": ")
-    messageText = para $ text (decodeUtf8 $ msgText msg)
-    nBlock = blocks <> monthPar <> senderPar <> messageText
+    monthFormat = formatTime defaultTimeLocale "%B %Y" $ localDay . time $ msg
+    monthH = if prevMonth == curMonth then mempty
+               else header 1 (text (T.pack monthFormat))
+    optionMaker n = (T.pack "", [T.pack n], [])
+    dateText = if prevDay == curDay then mempty
+                else divWith (optionMaker "date") $
+                     plain $ text $ T.pack $
+                     show curDay ++ "." ++ show curMonth ++ "."
+    senderText = if lSender == nSender then mempty
+                else divWith (optionMaker "sender") $
+                     plain $ text (decodeUtf8 nSender `T.append` T.pack ": ")
+    messagePar = divWith (optionMaker "msg") $
+                    plain $ text (decodeUtf8 $ msgText msg)
+    tod = localTimeOfDay . time $ msg
+    timeDay = show (todHour tod) ++ ":" ++ show (todMin tod)
+    timeText = divWith (optionMaker "time") $
+                    plain $ text $ T.pack timeDay
+    nBlock = blocks <> monthH <> dateText <> senderText <> messagePar <> timeText
 
 createDocument :: [Message] -> Pandoc
 createDocument msgs = doc $ blocks
